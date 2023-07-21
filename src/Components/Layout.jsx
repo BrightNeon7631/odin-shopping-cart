b895ec9d7df9269d19f96f007757ab3fbdb3d3c3
BrightNeon7631 <94930422+BrightNeon7631@users.noreply.github.com>
2023-07-22 00:10:17 +0200

    add remove from cart function and total price state to cart context provider

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import { 
     createContext, 
+    useEffect, 
     useState 
 } from 'react';
 
@@ -10,6 +11,15 @@ export const CartContext = createContext(null);
 
 export default function Layout() {
   const [cartItems, setCartItems] = useState([]);
+  const [totalOrderPrice, setTotalOrderPrice] = useState(0);
+
+  useEffect(() => {
+    setTotalOrderPrice(() => {
+        let total = 0;
+        cartItems.forEach(obj => total += obj.total);
+        return total;
+    })
+  }, [cartItems])
 
   function addToCart(product, quant) {
     setCartItems((prevState) => {
@@ -18,21 +28,27 @@ export default function Layout() {
         if (found) {
             const filterOutFound = prevState.filter(obj => obj.id != found.id);
             return [
-                { ...found, quantity: found.quantity + quant },
+                { ...found, quantity: found.quantity + quant, total: found.price * (found.quantity + quant) },
                 ...filterOutFound
             ]
         }
 
         return [
-            { ...product, quantity: quant },
+            { ...product, quantity: quant, total: quant * product.price },
             ...prevState
         ]
     })
   }
 
+  function removeFromCart(id) {
+    setCartItems((prevState) => {
+        return prevState.filter(obj => obj.id !== id);
+    })
+  }
+
   return (
     <div className="site-layout">
-      <CartContext.Provider value={{ cartItems, addToCart }}>
+      <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, totalOrderPrice }}>
         <Header />
         <Outlet />
       </CartContext.Provider>
