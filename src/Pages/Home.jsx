bb502a0011b65c073e04a101acd4ebad1a7eb575
BrightNeon7631 <94930422+BrightNeon7631@users.noreply.github.com>
2023-07-25 22:26:19 +0200

    add home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,16 @@
+import openImage from '../assets/images/open.jpg';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
     return (
-        <h1>This is a home page!</h1>
+        <div className='home-container'>
+            <div className='home-left'>
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+                <Link className='home-button' to='/store'>Shop now</Link>
+            </div>
+            <img className='home-img' src={openImage} />
+        </div>
     )
 }
