68a1de5a6bf399382339d65e1d2399e3d5faeba7
BrightNeon7631 <94930422+BrightNeon7631@users.noreply.github.com>
2023-07-31 15:20:13 +0200

    improve styling and add missing files

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -5,14 +5,14 @@ export default function Error() {
 
     if (error.message && error.statusText && error.status) {
         return (
-            <div className='error--container'>
+            <div className='error-container'>
                 <h1>Error: {error.message}</h1>
                 <pre>{error.status} : {error.statusText}</pre>
             </div>
         )
     } else {
         return (
-            <h1 className='error--container'>Unexpected Error</h1>
+            <h1 className='error-container'>Unexpected Error</h1>
         )
     }
 }
