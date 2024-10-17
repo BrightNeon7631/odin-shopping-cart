7437e956eef46b291fcba5d4ecd93d3e1973b88f
BrightNeon7631 <94930422+BrightNeon7631@users.noreply.github.com>
2024-10-17 19:26:32 +0200

    fix button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <div>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </div>
-                <Link className='home-button' to='/store'>Shop now</Link>
+                <Link className='store-link' to='/store'><button className='home-button'>Shop now</button></Link>
             </div>
             <img className='home-img' src={openImage} />
         </div>
